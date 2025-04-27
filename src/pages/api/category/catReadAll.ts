f2f94a91df41d13d6d1b6a2d65218fecc7902c40
yeittcl <raulzapatavillalobos@gmail.com>
2025-04-27 15:26:09 -0400

    Feat: se creo la API que devuelve todas las categorias

---
+++ src/pages/api/category/catReadAll.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '@/lib/prisma';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Metodo no permitido' });
+  }
+
+  try {
+    const categories = await prisma.category.findMany();
+
+    if (categories.length === 0) {
+        return res.status(404).json({ message: 'No se encontraron categorias' });
+      }
+
+    return res.status(200).json(categories);
+  } catch (error) {
+    return res.status(500).json({ error: {error} })
+  }
+}
