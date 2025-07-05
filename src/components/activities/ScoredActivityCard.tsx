4c6fea795f02e1742ad4db771d487e9e5601a22d
yeittcl <raulzapatavillalobos@gmail.com>
2025-07-05 16:59:06 -0400

    working star rating component

--- src/components/activities/ScoredActivityCard.tsx
+++ src/components/activities/ScoredActivityCard.tsx
@@ -1,13 +1,26 @@
-import { Typography } from "@mui/material";
+import React from "react";
+import { Typography, Box } from "@mui/material";
 import GenericActivityCard from "./GenericActivityCard";
 import LinearProgress from "@mui/material/LinearProgress";
 import { ScoredActivity } from "@/utils/calculateActivityScores";
+import StarRating from "@/components/rating/StarRating";
 
 type CardProps = {
   activity: ScoredActivity;
 }
 
 export default function ScoredActivityCard({ activity }: CardProps) {
+  const [rating, setRating] = React.useState(activity.rating || 0);
+
+  const handleRatingChange = async (newRating: number) => {
+    setRating(newRating);
+    await fetch(`/api/activity/rate`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: activity.id, rating: newRating }),
+    });
+  };
+
   return (
     <GenericActivityCard activity={activity}>
       <Typography
@@ -42,6 +55,9 @@ export default function ScoredActivityCard({ activity }: CardProps) {
           right: 8,
         }}
       />
+      <Box sx={{ position: "absolute", bottom: 8, right: 8 }}>
+        <StarRating value={rating} onChange={handleRatingChange} />
+      </Box>
     </GenericActivityCard>
   );
 }
